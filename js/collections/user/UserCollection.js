119bf59bbbc5fdf5ef32b85e5bf5157d20ea05f8
natsu90 <solomon_scorpio90@yahoo.com>
2015-03-01 10:44:31 +0800

    invalid data url

--- js/collections/user/UserCollection.js
+++ js/collections/user/UserCollection.js
@@ -1,8 +1,7 @@
 define(['jquery', 'underscore', 'backbone', 'models/user/UserModel'], function($, _, Backbone, UserModel) {
     var UserCollection = Backbone.Collection.extend({
         model: UserModel,
-        url: "http://localhost:8888/CRUD-worklight-api/get_all_users.php",
-        //url: "http://demo.revivalx.com/worklight/get_all_users.php",
+        url: "data/user.json",
         parse: function(data) {
             return data.users;
         }
